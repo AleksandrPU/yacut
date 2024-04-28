5c384cf70935dee87a4ae5fa3edbce52cdd2a6a5
Паутов Александр <sh2d000w@gmail.com>
2024-04-28 23:22:55 +0300

    fix: Add unique for short field. fix: Add index for short field and timestamp field.

--- yacut/models.py
+++ yacut/models.py
@@ -6,5 +6,5 @@ from . import db
 class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(256), nullable=False)
-    short = db.Column(db.String(16), nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    short = db.Column(db.String(16), unique=True, nullable=False, index=True)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
