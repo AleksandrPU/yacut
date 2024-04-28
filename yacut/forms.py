d0bf771024e35db01efdf6cf4b97cf8fba8d9d3b
Паутов Александр <sh2d000w@gmail.com>
2024-04-28 23:24:09 +0300

    fix: Add error messages for form validators. fix: Add regexp validator for custom_id field.

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,14 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, URLField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length, Optional, Regexp
 
 
 class URLMapForm(FlaskForm):
     original_link = URLField(
         'Длинная ссылка',
-        validators=[DataRequired()],
+        validators=[DataRequired(message='"url" является обязательным полем!')],
     )
     custom_id = StringField(
         'Ваш вариант короткой ссылки',
-        validators=[Length(1, 16), Optional()]
+        validators=[
+            Length(1, 16,
+                   message='Указано недопустимое имя для короткой ссылки'),
+            Regexp(r'[a-zA-Z0-9]+',
+                   message='Указано недопустимое имя для короткой ссылки'),
+            Optional()
+        ],
     )
