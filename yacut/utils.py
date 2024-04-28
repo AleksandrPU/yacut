e406cf23156b4c6bc99b57b7654d8fcc780fa56a
Паутов Александр <sh2d000w@gmail.com>
2024-04-28 23:34:24 +0300

    feat: Add get_unique_short_id in utils.py.

---
+++ yacut/utils.py
@@ -0,0 +1,12 @@
+from random import choices
+from string import ascii_letters, digits
+
+from yacut.models import URLMap
+
+
+def get_unique_short_id():
+    while True:
+        custom_id = ''.join(choices(ascii_letters + digits, k=6))
+        if URLMap.query.filter_by(custom_id=custom_id).first() is None:
+            break
+    return custom_id
