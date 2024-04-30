f5110d60ee0adae938c2aabd60dd3839e1be4544
Паутов Александр <sh2d000w@gmail.com>
2024-04-30 12:23:40 +0300

    feat: Add to_dict and from_dict to model.

--- yacut/models.py
+++ yacut/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from flask import request
+
 from . import db
 
 
@@ -8,3 +10,14 @@ class URLMap(db.Model):
     original = db.Column(db.String(256), nullable=False)
     short = db.Column(db.String(16), unique=True, nullable=False, index=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
+
+    def to_dict(self):
+        return dict(
+            url=self.original,
+            short_link=f'{request.url.removesuffix(request.path)}/{self.short}',
+        )
+
+    def from_dict(self, data):
+        for field in ['original', 'short']:
+            if field in data:
+                setattr(self, field, data[field])
