39b18520faa80e1fb3e11320c75fea3eaaec8ec5
Паутов Александр <sh2d000w@gmail.com>
2024-04-30 20:11:59 +0300

    fix: Move constant in constants.py.

--- yacut/utils.py
+++ yacut/utils.py
@@ -1,12 +1,15 @@
 from random import choices
 from string import ascii_letters, digits
 
+from yacut.constants import LENGTH_GENERATE_CUSTOM_ID
 from yacut.models import URLMap
 
 
 def get_unique_short_id():
     while True:
-        custom_id = ''.join(choices(ascii_letters + digits, k=6))
+        custom_id = ''.join(
+            choices(ascii_letters + digits, k=LENGTH_GENERATE_CUSTOM_ID)
+        )
         if URLMap.query.filter_by(short=custom_id).first() is None:
             break
     return custom_id
