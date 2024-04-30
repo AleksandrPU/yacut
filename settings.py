af4c6af2d662c36954068e128eda930a53f78148
Паутов Александр <sh2d000w@gmail.com>
2024-04-30 19:11:09 +0300

    fix: Add default values to getenv.

--- settings.py
+++ settings.py
@@ -7,6 +7,6 @@ load_dotenv()
 
 
 class Config(object):
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'flask')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
