e4b8ed3ff1d92e5ff052a580c7691c40c6c1291f
Паутов Александр <sh2d000w@gmail.com>
2024-04-30 12:19:41 +0300

    feat: Add views for API.

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -4,12 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 from settings import Config
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
-from . import error_handlers, views
+from . import api_views, error_handlers, views
---
+++ yacut/api_views.py
@@ -0,0 +1,51 @@
+from http import HTTPStatus
+
+from flask import jsonify, request
+from werkzeug.exceptions import BadRequest, NotFound
+
+from yacut import app, db
+from yacut.error_handlers import APIError
+from yacut.forms import URLMapForm
+from yacut.models import URLMap
+from yacut.utils import get_unique_short_id
+
+
+@app.route('/api/id/', methods=['POST'])
+def create_short_link():
+    data = request.get_json()
+    if data is None:
+        raise APIError(
+            'Отсутствует тело запроса', BadRequest
+        )
+
+    form = URLMapForm()
+    form.original_link.data = data.get('url', None)
+    form.custom_id.data = data.get('custom_id', None)
+    if not form.custom_id.data:
+        form.custom_id.data = get_unique_short_id()
+    form.validate()
+
+    for message in form.original_link.errors:
+        raise APIError(message, BadRequest)
+    for message in form.custom_id.errors:
+        raise APIError(message, BadRequest)
+
+    urlmap = URLMap()
+    urlmap.from_dict(
+        {
+            'original': form.original_link.data,
+            'short': form.custom_id.data,
+        }
+    )
+    db.session.add(urlmap)
+    db.session.commit()
+
+    return jsonify(urlmap.to_dict()), HTTPStatus.CREATED
+
+
+@app.route('/api/id/<string:short_id>/')
+def get_original_link(short_id):
+    original_link = URLMap.query.filter_by(short=short_id).first()
+    if original_link is None:
+        raise APIError('Указанный id не найден', NotFound)
+    return jsonify({'url': original_link.original}), HTTPStatus.OK
