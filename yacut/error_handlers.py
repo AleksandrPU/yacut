c6949f56106e8812faa375c3d159cbd3ee8ac7e1
Паутов Александр <sh2d000w@gmail.com>
2024-04-30 12:23:12 +0300

    feat: Add error handlers for API.

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -1,20 +1,44 @@
-from flask import render_template
-from werkzeug.exceptions import InternalServerError, NotFound
+from flask import jsonify, render_template
+from werkzeug.exceptions import InternalServerError, MethodNotAllowed, NotFound
 
 from yacut import app
 
 
+class APIError(Exception):
+
+    def __init__(self, message, status=None):
+        super().__init__()
+        self.message = message
+        if status:
+            self.status_code = status.code
+
+    def to_dict(self):
+        return dict(message=self.message)
+
+
+@app.errorhandler(APIError)
+def api_error(error):
+    return jsonify(error.to_dict()), error.status_code
+
+
+@app.errorhandler(MethodNotAllowed)
+def method_not_allowed(error):
+    return jsonify({'message': 'Метод не доступен'}), error.code
+
+
 @app.errorhandler(NotFound)
 def not_found(error):
-    message = 'Такой короткой ссылки ещё не создали.'
     return render_template(
-        'error.html', message=message, code=error.code,
+        'error.html',
+        message='Такой короткой ссылки ещё не создали.',
+        code=error.code,
     ), error.code
 
 
 @app.errorhandler(InternalServerError)
 def internal_server_error(error):
-    message = 'Что-то пошло не так. Попробуйте ещё раз, пожалуйста.'
     return render_template(
-        'error.html', message=message, code=error.code,
+        'error.html',
+        message='Что-то пошло не так. Попробуйте ещё раз, пожалуйста.',
+        code=error.code,
     ), error.code
