b95df00baff64af416cd88145e5d17ec8fa12b99
Паутов Александр <sh2d000w@gmail.com>
2024-05-04 19:46:24 +0300

    fix: Refactor __init__.py project.

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,37 +1,26 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
 
 from settings import Config
 
 
-class Base(DeclarativeBase):
-    pass
-
-
-db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy()
 migrate = Migrate()
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
     migrate.init_app(app, db)
 
     from . import api_views
     app.register_blueprint(api_views.bp)
-    # todo
-    # app.add_url_rule('/', endpoint='')
 
     from . import views
     app.register_blueprint(views.bp)
     app.add_url_rule('/', endpoint='index_view')
-
-    # todo
-    # from . import error_handlers
-    # app.register_blueprint(error_handlers.bp)
 
     return app
